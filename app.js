831682e74171ee5d2c68b9dc786d4ab4a482020d
Alfred Wu <alfredwuy@gmail.com>
2017-12-01 17:40:11 +0000

    for github test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/scenicspots", function(req, res) {
     var scenicspots = [
         {name: "1", image: "https://farm6.staticflickr.com/5604/15541744427_3d92a480a4.jpg"},
         {name: "2", image: "https://farm6.staticflickr.com/5641/22403300756_a413f3fd59.jpg"},
-        {name: "3", image: "https://farm5.staticflickr.com/4308/36053252101_dde14a28a9.jpg"}
+        // {name: "3", image: "https://farm5.staticflickr.com/4308/36053252101_dde14a28a9.jpg"}
     ]
     res.render("scenicspots", {scenicspots:scenicspots});
 })
