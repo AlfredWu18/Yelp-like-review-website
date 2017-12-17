be5e35734b25eef933ab5ebbde5b70f03d56f751
AlfredWu18 <yuw681@eng.ucsd.edu>
2017-12-17 20:09:35 +0000

    add show page

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ app.get("/", function(req, res) {
 
 var spotSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var scenicspot = mongoose.model("scenicspot", spotSchema);
 
 // scenicspot.create({
-//     name: "2", image: "https://farm6.staticflickr.com/5641/22403300756_a413f3fd59.jpg"
+//     name: "2", image: "https://farm6.staticflickr.com/5641/22403300756_a413f3fd59.jpg",
+//     description: "beautiful maintain view"
 // }, function(err, scenicspot) {
 //     if (err) {
 //         console.log(err);
@@ -39,7 +41,7 @@ app.get("/scenicspots", function(req, res) {
         if (err) {
             console.log();
         } else {
-            res.render("scenicspots", {scenicspots:allScenicspot});
+            res.render("index", {scenicspots:allScenicspot});
         }
     });
 });
@@ -51,12 +53,25 @@ app.get("/scenicspots/new", function(req, res){
 app.post("/scenicspots", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newScenicSpot = {name: name, image:image};
+    var desc = req.body.description;
+    var newScenicSpot = {name: name, image:image, description: desc};
     scenicspot.create(newScenicSpot, function(err, newlyCreated) {
         if (err) {
             console.log(err);
         } else {
             res.redirect("/scenicspots");
+        }
+    })
+})
+
+
+app.get("/scenicspots/:id", function(req, res) {
+    // res.send("this will be");
+    scenicspot.findById(req.params.id, function(err, foundScenicspot) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {scenicspot: foundScenicspot});
         }
     })
 })
