57d6a863b7141540ba7e622f2439c09a8df5ec31
AlfredWu18 <yuw681@eng.ucsd.edu>
2017-12-20 06:11:13 +0000

    comment seeds.js code

--- seeds.js
+++ seeds.js
@@ -2,27 +2,27 @@ var mongoose        = require("mongoose"),
     scenicspot      = require("./models/scenicspot"),
     Comment         = require("./models/comment");
 
-var data = [
-    {
-        name: "Fanta Island", 
-        image: "https://farm6.staticflickr.com/5641/22403300756_a413f3fd59.jpg",
-        description: "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more."
-    },
-    {
-        name: "Desert Mesa", 
-        image: "https://farm6.staticflickr.com/5604/15541744427_3d92a480a4.jpg",
-        description: "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more."
-    },
-    {
-        name: "Cloud's Rest", 
-        image: "https://farm5.staticflickr.com/4308/36053252101_dde14a28a9.jpg",
-        description: "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more."
-    },
-]
+// var data = [
+//     {
+//         name: "Fanta Island", 
+//         image: "https://farm6.staticflickr.com/5641/22403300756_a413f3fd59.jpg",
+//         description: "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more."
+//     },
+//     {
+//         name: "Desert Mesa", 
+//         image: "https://farm6.staticflickr.com/5604/15541744427_3d92a480a4.jpg",
+//         description: "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more."
+//     },
+//     {
+//         name: "Cloud's Rest", 
+//         image: "https://farm5.staticflickr.com/4308/36053252101_dde14a28a9.jpg",
+//         description: "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more."
+//     },
+// ]
 
 function seedDB() {
     //remove all scenicspots
-    scenicspot.remove({}, function(err) {
+    // scenicspot.remove({}, function(err) {
         // if (err) {
         //     console.log(err);
         // }
@@ -51,7 +51,7 @@ function seedDB() {
         //         }
         //     });
         // });
-    });
+    // });
 }
 
 module.exports = seedDB;
